e87c0eb4ca3866a1d63c8b21db0e1ea89cc27bb4
Eric King <eric.king@lonelyplanet.com>
2017-10-09 15:58:16 -0500

    Added pre-commit hook to run PHPCS, PHPMD, PHPUnit

---
+++ tests/DemoTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace webdeveric\WPStarter\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class DemoTest extends TestCase
+{
+    public function testDemo()
+    {
+        $this->assertTrue(true);
+    }
+}
